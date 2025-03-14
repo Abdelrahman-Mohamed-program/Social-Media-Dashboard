3336ca6c9b6c443be7548f8b51b6942b8678fb7f
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2025-03-14 17:09:54 +0200

    Conditional navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../services/AuthService";
 
 function Navbar() {
+  // const [token, setIsAuthenticated] = useState(false);
+  const { token } = useAuth();
+
   return (
     <>
       <nav className="navbar navbar-expand-lg text-light navbar-dark bg-dark">
@@ -9,19 +13,32 @@ function Navbar() {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse " id="navbarNav">
-            <ul className="navbar-nav d-flex w-100 justify-content-end ">
-              <li className="nav-item text-light mx-2">
-                <Link className="nav-link fs-5" to="/">Home</Link>
-              </li>
-              <li className="nav-item mx-2">
-                <Link className="nav-link fs-5 " to="/login">Log in</Link>
-              </li>
-              <li className="nav-item mx-2 ">
-                <Link className="nav-link fs-5" to="/signup">Sign up</Link>
-              </li>
-            </ul>
-          </div>
+
+          {!token ?
+            <div className="collapse navbar-collapse " id="navbarNav">
+              <ul className="navbar-nav d-flex w-100 justify-content-end ">
+                <li className="nav-item text-light mx-2">
+                  <Link className="nav-link fs-5" to="/">Home</Link>
+                </li>
+                <li className="nav-item mx-2">
+                  <Link className="nav-link fs-5 " to="/login">Log in</Link>
+                </li>
+                <li className="nav-item mx-2 ">
+                  <Link className="nav-link fs-5" to="/signup">Sign up</Link>
+                </li>
+              </ul>
+            </div> :
+            <div>
+              <ul className="navbar-nav d-flex w-100 justify-content-end ">
+                <li className="nav-item text-light mx-2">
+                  <Link className="nav-link fs-5" to="/">Home</Link>
+                </li>
+                <li className="nav-item mx-2">
+                  <Link className="nav-link fs-5 " to="/profile">Profile</Link>
+                </li>
+              </ul>
+            </div>
+          }
         </div>
       </nav>
     </>
