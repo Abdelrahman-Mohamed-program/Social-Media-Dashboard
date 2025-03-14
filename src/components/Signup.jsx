fd71e8b5dffca097353b1e957c8b70da8d5afe2a
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2025-03-14 17:27:12 +0200

    Signup - draft

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,66 +1,79 @@
 import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { AddUser, GetUsers } from "../../service/users.service";
+import useAxiosInstance from "../config/api";
+import { useAuth } from "../services/AuthService";
 
 function Singup() {
+  const { axios } = useAxiosInstance();
+  const { setToken } = useAuth();
 
-
-  const [User, SetUser] = useState({
+  const [User, setUser] = useState({
     user_name: "",
     email: "",
     password: "",
     logged_in: false
   }
   )
-  const [Users, SetUsers] = useState([])
-  const [CheckUser, SetCheckUser] = useState(false)
-  const [ConfirmPassword, SetCofirmPassword] = useState("")
-  const [CheckPassword, SetCheckPassword] = useState(true)
-  const [CheckEmpty, SetCheckEmpty] = useState(false)
+  const [Users, setUsers] = useState([])
+  const [CheckUser, setCheckUser] = useState(false)
+  const [ConfirmPassword, setCofirmPassword] = useState("")
+  const [CheckPassword, setCheckPassword] = useState(true)
+  const [CheckEmpty, setCheckEmpty] = useState(false)
 
 
 
 
   useEffect(() => {//gets users from the end point and focuses on the first input when rendering
-    GetUsers().then(res => SetUsers(res.data))
+    GetUsers().then(res => setUsers(res.data))
 
   }, [])
 
   useEffect(() => {//remove the already regestered alert when typing the email again
-    SetCheckUser(false)
+    setCheckUser(false)
   }, [User.email])
 
 
   useEffect(() => {//remove the effect of empty alert when typing again
     if (User.password != "" || ConfirmPassword != "" || User.email != "" || User.user_name != "") {
-      SetCheckEmpty(false)
+      setCheckEmpty(false)
     }
   }, [User, ConfirmPassword])
 
 
 
   useEffect(() => {//remove the effect of comfirmed password alert when typing password again
-    SetCheckPassword(true)
+    setCheckPassword(true)
   }, [User.password, ConfirmPassword])
 
 
   async function sign_up(event) {
     event.preventDefault()
     if (User.password == "" || ConfirmPassword == "" || User.email === "" || User.user_name == "") {
-      SetCheckEmpty(true)
+      setCheckEmpty(true)
     } else {
       if ((User.password != "" && ConfirmPassword == "") || (ConfirmPassword != "" && User.password !== ConfirmPassword)) {
-        SetCheckPassword(false)
+        setCheckPassword(false)
       } else {
-        if (Users.filter(u => u.email === User.email).length == 1) {//check if user is aleardy in the data base
-          SetCheckUser(true)
-        }
-        else {//will add user if first time signing up with email
-          const updatedUser = { ...User, logged_in: true };
-          SetUser(updatedUser);
-          await AddUser(updatedUser);
-          GetUsers().then(res => SetUsers(res.data))
-        }
+        // if (Users.filter(u => u.email === User.email).length == 1) {//check if user is aleardy in the data base
+        //   setCheckUser(true)
+        // }
+        // else {//will add user if first time signing up with email
+        //   // const updatedUser = { ...User, logged_in: true };
+        //   // setUser(updatedUser);
+        //   // await AddUser(updatedUser);
+        //   // GetUsers().then(res => setUsers(res.data))
+
+        // }
+
+        axios.post("/auth/register", User)
+          .then(res => res.data)
+          .then(data => {
+            console.log(data);
+          })
+          .catch(err => {
+            console.log(err);
+          })
       }
     }
   }
@@ -85,13 +98,13 @@ function Singup() {
             <form onSubmit={sign_up} >
               <div className="mb-3">
                 <label htmlFor="username" className="form-label">Username</label>
-                <input type="text" id="username" className="form-control" onChange={e => SetUser({ ...User, user_name: e.target.value })}
+                <input type="text" id="username" className="form-control" onChange={e => setUser({ ...User, user_name: e.target.value })}
                   placeholder="Enter your name" />
               </div>
 
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">Email</label>
-                <input type="email" autoComplete="off" id="email" className="form-control" onChange={(e) => { SetUser({ ...User, email: e.target.value }) }}
+                <input type="email" autoComplete="off" id="email" className="form-control" onChange={(e) => { setUser({ ...User, email: e.target.value }) }}
                   placeholder="Enter your email" />
                 {(CheckUser) && <div className="alert alert-danger" role="alert">
                   this email is already Registered please  <Link to="/login" className="text-primary text-decoration-none fw-semibold"> Log in</Link>.
@@ -101,13 +114,13 @@ function Singup() {
 
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" id="password" className="form-control" onChange={e => SetUser({ ...User, password: e.target.value })}
+                <input type="password" id="password" className="form-control" onChange={e => setUser({ ...User, password: e.target.value })}
                   placeholder="Enter your password" />
               </div>
 
               <div className="mb-5">
                 <label htmlFor="confirmPassword" className="form-label">Repeat Password</label>
-                <input type="text" id="confirmPassword" className="form-control" onChange={e => (SetCofirmPassword(e.target.value))} placeholder="Confirm your password" />
+                <input type="text" id="confirmPassword" className="form-control" onChange={e => (setCofirmPassword(e.target.value))} placeholder="Confirm your password" />
                 {(!CheckPassword) && <div className="alert alert-danger" role="alert">
                   please re-enter the password correctly
                 </div>}
