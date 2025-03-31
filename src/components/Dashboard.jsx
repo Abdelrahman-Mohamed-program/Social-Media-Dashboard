52f39a501abaab038a35ac2394d5d25b2836b4b3
Mostafa-Ehab <mostafa.e.h.m.r@gmail.com>
2025-03-31 04:21:48 +0200

    Fix chart rendering

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { formatNumber } from "../utils/numUtils";
+import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
 import useAxiosInstance from "../config/api";
 
